57f8bac77ece074f09cf4cff286399f69d5af1ef
TheLlama1 <138434002+TheLlama1@users.noreply.github.com>
2024-10-02 22:55:24 +0300

    Add files via upload

---
+++ WebApplication1/WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) { }
+        public DbSet <Category> WebApplication1 { get; set; }
+    }
+}
